b561ea31aea6b8d3f3baca32825fc266a7536f83
Kunmitech1 <olumuyiwa761@gmail.com>
2026-06-23 02:26:03 +0100

    Add Firebase configuration file
    This file contains the Firebase configuration settings required for the application.

---
+++ firebase config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+// ⚠️  Add this file to .gitignore — do NOT commit to GitHub
+const firebaseConfig = {
+  apiKey: "AIzaSyDLYshhbmb5YJJotR2Mui2ut-MPzKas7IQ",
+  authDomain: "farm-ting.firebaseapp.com",
+  databaseURL: "https://farm-ting-default-rtdb.firebaseio.com",
+  projectId: "farm-ting",
+  storageBucket: "farm-ting.firebasestorage.app",
+  messagingSenderId: "679853844895",
+  appId: "1:679853844895:web:5e7a8eafa19806f8bf35aa"
+};
